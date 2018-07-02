c0db95cc0d4c3e1ab8adceec2d7c9a896eabaa3a
Unknown <margaridaCostaFaria@gmail.com>
2018-07-02 13:41:13 +0200

    Parameter name collision.

--- src/free_cloud_node.cpp
+++ src/free_cloud_node.cpp
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	
 	std::string hokuyo_topic;
-	nh.getParam("hokuyo_topic", hokuyo_topic);
+	nh.getParam("uav/laser_topic", hokuyo_topic);
 	ros::Subscriber laser_scan_sub = nh.subscribe<sensor_msgs::LaserScan>(hokuyo_topic, 1, free_cloud_node::free_cloud_cb);
     free_cloud_node::free_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("free_cloud", 10);
 	
